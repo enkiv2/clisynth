8b10b79156bae38cc1b5603060b9961f00196a44
John Ohno <john.ohno@gmail.com>
2023-04-28 12:10:01 -0400

    split out more knobs (necessitating that we no longer use the neos helper function and instead use synth itself)

--- synthmorse.sh
+++ synthmorse.sh
@@ -2,16 +2,16 @@
 dit="B"
 daw="C"
 gain=-23
+tempo=8.0
+octave=3
 
 . ./clisynth.sh
 
-while read x ; do 
-	neos $(
-		echo "$x" | 
-			morse | 
-			sed 's/dit/'"$dit"'/g;s/daw/'"$daw"'/g' | 
-			tr '\n' 'R' | 
-			sed 's/\([A-Z]\)R/\1 R/g;s/RR/R R/g'
-	)
-done | play -p gain $gain
+morse | 
+	sed 's/dit/'"$dit"'/g;s/daw/'"$daw"'/g' | # dit & daw become notes
+	tr '\n' 'R' | sed 's/\([A-Z]\)R/\1 R/g;s/RR/R R/g' | # newlines become rests
+	tr ' ' '\n' | # prepare for synthesis
+		while read x ; do
+			synth $((rat/tempo)) $x$octave
+		done | play -p gain $gain
 
